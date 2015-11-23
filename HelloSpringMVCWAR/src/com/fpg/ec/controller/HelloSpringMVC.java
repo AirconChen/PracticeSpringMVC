8870daa687e97011b30018fb03455888b14c1395
ChenHsinHsuan <aircon.chen.apple@gmail.com>
2015-11-23 10:03:04 +0800

    demo finish

--- HelloSpringMVCWAR/src/com/fpg/ec/controller/HelloSpringMVC.java
+++ HelloSpringMVCWAR/src/com/fpg/ec/controller/HelloSpringMVC.java
@@ -3,13 +3,22 @@ package com.fpg.ec.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 
 @Controller
-@RequestMapping("hello/**")
+@RequestMapping("/hello")
 public class HelloSpringMVC {
-	@RequestMapping("print/{name}")
-	public String printHello(@PathVariable("name") String iName){
+	
+	
+	@RequestMapping(value="/print", method=RequestMethod.GET)
+	public String printHello(){
+		return "hello";
+	}
+	
+	
+	@RequestMapping("/print/{name}")
+	public String printHello2(@PathVariable("name") String iName){
 		System.out.println("iName:"+iName);
 		return "hello";
 	}
